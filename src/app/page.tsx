eacb863d1b2df26b2b51e178d9826d3e23b74afa
ajoshi <ajoshi@bosaproperties.com>
2026-04-24 02:58:37 +0530

    added tailwind variants to remaining files.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,36 @@
 import populationByYear from "@/lib/data/population-by-year.json";
+import { tv } from "tailwind-variants";
 import { PopulationYearEntry } from "@/types/population";
 import PopulationViewer from "@/components/populaiton/PopulationViewer";
 
 export default function Home() {
+  const { main, contentCard } = HOME_PAGE_VARIANTS();
+
   // Convert the population data to the PopulationYearEntry type
   const populationData = populationByYear as PopulationYearEntry[];
 
   return (
-    <>
-    <main className="min-h-screen bg-neutral-100 px-4 py-10 sm:px-6">
-    <div className="mx-auto max-w-3xl rounded-lg border border-neutral-200 bg-white p-5 shadow-sm sm:p-7">  
-      <PopulationViewer data={populationData} />
-    </div>
+    <main className={main()}>
+      <div className={contentCard()}>
+        <PopulationViewer data={populationData} />
+      </div>
     </main>
-    </>
   );
 }
+
+const HOME_PAGE_VARIANTS = tv({
+  slots: {
+    main: ["min-h-screen", "bg-neutral-100", "px-4", "py-10", "sm:px-6"],
+    contentCard: [
+      "mx-auto",
+      "max-w-3xl",
+      "rounded-lg",
+      "border",
+      "border-neutral-200",
+      "bg-white",
+      "p-5",
+      "shadow-sm",
+      "sm:p-7",
+    ],
+  },
+});
